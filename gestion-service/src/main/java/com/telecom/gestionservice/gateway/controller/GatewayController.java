13f89509eff4dac6186e5ef12afa4cceb3db6e6f
malki13 <malkiyupanki12@gmail.com>
2024-02-16 11:11:15 -0500

    actualizacion gestion

--- gestion-service/src/main/java/com/telecom/gestionservice/gateway/controller/GatewayController.java
+++ gestion-service/src/main/java/com/telecom/gestionservice/gateway/controller/GatewayController.java
@@ -1,5 +1,12 @@
 package com.telecom.gestionservice.gateway.controller;
 
+import com.telecom.gestionservice.gateway.data.info.GatewayInfo;
+import com.telecom.gestionservice.gateway.service.GatewayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,11 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/gestion")
 public class GatewayController {
+    @Autowired
+    private GatewayService gatewayService;
     @GetMapping("/{id}")
     public ResponseEntity<String> getById(@PathVariable("id") int id) {
 //        Bike bike = bikeService.getBikeById(id);
 //        if(bike == null)
 //            return ResponseEntity.notFound().build();
         return ResponseEntity.ok("Hola"+String.valueOf(id));
+    }
+    @GetMapping("/getAll")
+    public ResponseEntity<Page<GatewayInfo>> findAll(@PageableDefault(size = 10, page = 0) Pageable pageable) {
+        return new ResponseEntity<>(gatewayService.findAll(pageable), HttpStatus.OK);
     }
 }
