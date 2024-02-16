32f92afb2cf800a20a3524a134df4671b4ae85a2
malki13 <malkiyupanki12@gmail.com>
2024-02-16 09:59:29 -0500

    actualizacion gateway

--- bike-service/src/main/java/com/tutorial/bikeservice/entity/Bike.java
+++ bike-service/src/main/java/com/tutorial/bikeservice/entity/Bike.java
@@ -1,18 +1,22 @@
 package com.tutorial.bikeservice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class Bike {
 
     @Id
@@ -21,4 +25,46 @@ public class Bike {
     private String brand;
     private String model;
     private int userId;
+
+    public Bike() {
+    }
+
+    public Bike(int id, String brand, String model, int userId) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
